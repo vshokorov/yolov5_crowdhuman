9174a27f893e8cce9f6558af5a2ff134f5b5fec5
Your Name <you@example.com>
2022-08-11 16:36:04 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import pathlib
+import pkg_resources
+
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+setup(
+    name='yolov5_crowdhuman',
+    author='vshokorov',
+    author_email='some_mail@gmail.com',
+    url="",
+    version='0.0.1',
+    install_requires=install_requires,
+)
